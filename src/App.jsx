3cd37bf41df01c85b2013ab4c8520bdcc6e6a95e
Winston Jimenez <devadmin@barriowealth.com>
2025-12-02 18:52:05 -0500

    From local to production

--- src/App.jsx
+++ src/App.jsx
@@ -21,7 +21,7 @@ import {
  const API_URL = import.meta.env.VITE_API_URL || "http://localhost:5000/api";
 
 // 🅱️ LOCAL/PREVIEW (Active for now):
-const API_URL = "http://localhost:5000/api";
+//const API_URL = "http://localhost:5000/api";
 
 
 // --- CLERK KEY ---
